0f81d8686840ee2131d9c57a795903810fe46184
nithin stany dsouza <nithinstany@gmail.com>
2010-04-27 09:55:04 +0530

    chanegs to gallery helper : dynamic record

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -3,6 +3,8 @@ module  GalleriesHelper
   link_to_function "Add Image ", :id  => "add_photo" do |page|
     form_builder.fields_for :gallery_assets, GalleryAsset.new, :child_index => 'NEW_RECORD' do |photo_form|
       html = render(:partial => 'gallery_asset', :locals => { :f => photo_form })
+      # content = escape_javascript(html)
+      # page << "$('#{content}'.replace(/NEW_RECORD/g , new Date().getTime())).insertBefore('#add_site_detail')" if using jQuery
       page << "$('add_photo').insert({ after: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) }); "
     end
   end
